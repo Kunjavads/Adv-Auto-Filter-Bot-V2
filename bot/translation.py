187e4ec24053161b062e5e23b3254d4cb30ea62f
Kunjavads <90408477+Kunjavads@users.noreply.github.com>
2021-09-11 12:18:30 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -7,18 +7,17 @@ class Translation(object):
     START_TEXT = """<b>Hey {}!!</b>
 <i>Am Just A Advance Auto Filter Bot....😉
 
-Just Add Me To Your Group And Channel And Connect Them And See My Pevers 🔥🔥😝
+വെറുതെ നോക്കി സമയം കളയണ്ട @movie_house2 ഗ്രൂപ്പിൽ മാത്രേമേ ഈ ബോട്ട് ഉപയോഗിക്കാൻ പറ്റുകയുള്ളു..!😜😜
+━═━═━═━═━═━═━═━═━═
 
 For More Details Click Help Button Below..
-@CrazyBotsz
+@movie_house2
 </i>"""    
     
     HELP_TEXT = """
 <b><i><u>How To Use Me!?</u></i></b>
 
 <i>
--> Add Me To Any Group And Make Me Admin
--> Add Me To Your Desired Channel
 </i>
 
 <b>Bot Commands (Works Only In Groups) :</b>
@@ -43,16 +42,16 @@ For More Details Click Help Button Below..
             
             -> <code>Status</code> - Button Will Shows The Stats Of Your Channel
             
-@CrazyBotsz
+@movie_house2
 """
     
     ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
     
-<b>➥ Creator</b> : <b><i><a href="https://t.me/AlbertEinstein_TG">AlbertEinstein_TG</a></i></b>
+<b>➥ Creator</b> : <b><i><a href="https://t.me/DS_KUNJAVA"DS_KUNJAVA</a></i></b>
 
 <b>➥ Language</b> : <code>Python3</code>
 
 <b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
 
-<b>➥ Source Code</b> : <i><a href="https://github.com/AlbertEinsteinTG/Adv-Auto-Filter-Bot">Click Me</a></i>
+<b>➥ Source Code</b> : <i><a href="https://github.com/Kunjavads/Adv-Auto-Filter-Bot-V2.git">Click Me</a></i>
 """
